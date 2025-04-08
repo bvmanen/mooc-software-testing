ccccaf577266267a71f08d8c2a625d293bbacc66
bvmanen <bastiaanvanmanen@gmail.com>
2025-04-08 13:14:50 +0200

    Update LeapYearTest.java

--- src/test/java/tudelft/leapyear/LeapYearTest.java
+++ src/test/java/tudelft/leapyear/LeapYearTest.java
@@ -4,28 +4,31 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class LeapYearTest {
+    
+        private LeapYear ly;
+
+    @BeforeEach
+    public void initialize() {
+        ly = new LeapYear();
+    }
+    
     @Test
     public void leapYearsThatAreNonCenturialYears() {
-        LeapYear ly = new LeapYear();
-        boolean result = ly.isLeapYear(2016);
-        Assertions.assertTrue(result);
+        Assertions.assertTrue(ly.isLeapYear(2016));
     }
 
     @Test
     public void leapCenturialYears() {
-        LeapYear ly = new LeapYear();
         Assertions.assertTrue(ly.isLeapYear(2000));
     }
 
     @Test
     public void nonLeapCenturialYears() {
-        LeapYear ly = new LeapYear();
         Assertions.assertFalse(ly.isLeapYear(1500));
     }
 
     @Test
     public void nonLeapYears() {
-        LeapYear ly = new LeapYear();
         Assertions.assertFalse(ly.isLeapYear(2017));
     }
 }
