87ce769c4708c293c318071d76bbae459bbb6633
bvmanen <bastiaanvanmanen@gmail.com>
2025-04-09 14:08:50 +0200

    Update DiscountApplierTest.java

--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -2,4 +2,21 @@ package tudelft.discount;
 
 public class DiscountApplierTest {
 
+    @Test
+    void discountApplier() {
+
+        Product one = new Product("Shoe", 20.0, "BUSINESS");
+        Product two = new Product("Plate", 300.0, "HOME");
+
+        ProductDao dao = Mockito.mock(ProductDao.class);
+
+        List<Product> results = Arrays.asList(one, two);
+        Mockito.when(dao.all()).thenReturn(results);
+
+        DiscountApplier da = new DiscountApplier(dao);
+        da.setNewPrices();
+
+        Assertions.assertEquals(22, results.get(0).getPrice());
+        Assertions.assertEquals(270, results.get(1).getPrice());
+    }
 }
